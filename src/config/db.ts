982500aae1dacebd3badfa16339a399cc56e05b1
kamandanu <kamandanug@gmail.com>
2025-07-13 21:16:41 +0700

    feat: setup config for cloud

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,15 +2,19 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     timezone: "+07:00",
-    logging: true,
+    logging: !isProduction,
   }
 );
 
